955d64a94a8f49c0e1cd06fd57772941619860ca
Mark Cotner <mark.cotner@gmail.com>
2025-11-23 19:54:16 -0500

    pushing 0.8.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bindocsis.MixProject do
   def project do
     [
       app: :bindocsis,
-      version: "0.8.1",
+      version: "0.8.2",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
